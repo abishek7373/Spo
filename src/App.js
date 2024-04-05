4566e5ee1dc0c1ba7a03b6403b88d4eb70dc1525
abishek7373 <abisheakmarudhus@gmail.com>
2024-04-05 15:27:03 +0530

    Welcome

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Routes,  Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import Home from './Components/Home/Home';
+import Register from './Components/Register/Register';
+import Profile from './Components/Profile/Profile';
+import Search from './Components/Search/Search';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+     </Router>
+     <Footer />
+   </div>
   );
 }
 
